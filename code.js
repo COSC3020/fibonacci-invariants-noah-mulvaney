7f4dfd315d4e82ba8f5d4d112177d9525e44063d
Noah Mulvaney <127539141+noah-mulvaney@users.noreply.github.com>
2024-04-05 15:17:15 -0600

    Implement recursive Fibonacci

--- code.js
+++ code.js
@@ -1,3 +1,27 @@
-function fib(n) {
-    return n;
+// COSC3020 Fibonacci Invariants
+// Noah Mulvaney
+// 5 Apr 2024
+
+function fibr(n, arr) {
+    if (n <= 0) {
+        arr[0] = 0;
+        return 0;
+    }
+    
+    if (n <= 2) {
+        arr[n] = 1;
+        return 1;
+    }
+    
+    arr[n] = fibr(n - 1, arr) + fibr(n - 2, arr);
+    return arr[n];
 }
+
+function fib(n) {
+    return fibr(n, []);
+}
+
+/* Debug Code
+for (let i = 0; i < 10; ++i)
+    console.log(fib(i));
+*/
